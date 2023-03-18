50ea0ccd320a54f987d3159c2277196934f6d0e2
DDD <Sabitov.daniyar18@mail.ru>
2023-03-18 15:32:03 +0300

    create .jar with dependencies

--- src/main/java/com/sabitov/Main2.java
+++
@@ -1,7 +0,0 @@
-package com.sabitov;
-
-public class Main2 {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-mvn compile
-mvn -e exec:java -Dexec.mainClass='C:\Users\Пользователь\Desktop\2course\JavaLab\IdeaPlatformTest\src\main\java\com\sabitov\Main.java'
+java -jar ./target/IdeaPlatformTest-1.0-SNAPSHOT.jar
